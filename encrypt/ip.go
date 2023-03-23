8091fc892b558af185fd203d74ccabd8a5f568c9
narsi <narsihuang@gmail.com>
2023-03-23 16:46:51 +0800

    优化 ip 加密

--- encrypt/ip.go
+++ encrypt/ip.go
@@ -1,54 +1,80 @@
 package encrypt
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"errors"
-	"math"
 	"net"
-	"strconv"
 )
 
-var ipEncryptKey uint64 = 42
+const (
+	ipv4len         = net.IPv4len
+	ipEncryptKeyLen = 16
+	ipEncryptKeyDef = "rNd3TQq9BdTAKF42"
+)
 
-func IpEncrypt(ip string) string {
-	ipInt, err := IPString2Long(ip)
+var ErrIPBeyondIPv4 = errors.New("IP address beyond the scope of IPv4")
+var ErrInvalidIPHex = errors.New("invalid IP address hex")
+
+func newIPKey() ([]byte, error) {
+	key := make([]byte, ipEncryptKeyLen)
+	_, err := rand.Read(key)
 	if err != nil {
-		return ""
+		return nil, err
 	}
-	return strconv.FormatUint(uint64(ipInt)+ipEncryptKey, 16)
+	return key, nil
 }
 
-func IpDecrypt(ipStr string) string {
-	ipInt, err := strconv.ParseUint(ipStr, 16, 64)
-
-	if err != nil {
-		return ""
-	}
-	ip, err := Long2IPString(uint(ipInt - ipEncryptKey))
-	if err != nil {
-		return ""
-	}
-	return ip
+type IPEncrypter struct {
+	key []byte
 }
 
-func IPString2Long(ip string) (uint, error) {
-	b := net.ParseIP(ip).To4()
-	if b == nil {
-		return 0, errors.New("beyond the scope of ipv4")
-	}
-
-	return uint(b[3]) | uint(b[2])<<8 | uint(b[1])<<16 | uint(b[0])<<24, nil
+func NewIPEncrypter() *IPEncrypter {
+	return &IPEncrypter{key: []byte(ipEncryptKeyDef)}
 }
 
-func Long2IPString(i uint) (string, error) {
-	if i > math.MaxUint32 {
-		return "", errors.New("beyond the scope of ipv4")
+func (enc *IPEncrypter) Encrypt(ip string) (string, error) {
+	ipBytes := net.ParseIP(ip).To4()
+	if ipBytes == nil {
+		return "", ErrIPBeyondIPv4
 	}
+	ipUint32 := uint32(ipBytes[0])<<24 | uint32(ipBytes[1])<<16 | uint32(ipBytes[2])<<8 | uint32(ipBytes[3])
+	ipUint64 := uint64(ipUint32) + enc.ipEncryptKey()
+	ipHex := hex.EncodeToString(enc.encodeIPUint64(ipUint64))
+	return ipHex, nil
+}
 
-	ip := make(net.IP, net.IPv4len)
-	ip[0] = byte(i >> 24)
-	ip[1] = byte(i >> 16)
-	ip[2] = byte(i >> 8)
-	ip[3] = byte(i)
-
+func (enc *IPEncrypter) Decrypt(ipHex string) (string, error) {
+	if ipHex == "" {
+		return "", nil
+	}
+	ipBytes, err := hex.DecodeString(ipHex)
+	if err != nil {
+		return "", ErrInvalidIPHex
+	}
+	ipUint64 := enc.decodeIPUint64(ipBytes)
+	ipUint32 := uint32(ipUint64 - enc.ipEncryptKey())
+	ip := net.IPv4(byte(ipUint32>>24), byte(ipUint32>>16), byte(ipUint32>>8), byte(ipUint32))
 	return ip.String(), nil
+}
+
+func (enc *IPEncrypter) ipEncryptKey() uint64 {
+	return enc.decodeIPUint64(enc.key)
+}
+
+func (enc *IPEncrypter) encodeIPUint64(ipUint64 uint64) []byte {
+	b := make([]byte, 8)
+	b[0] = byte(ipUint64 >> 56)
+	b[1] = byte(ipUint64 >> 48)
+	b[2] = byte(ipUint64 >> 40)
+	b[3] = byte(ipUint64 >> 32)
+	b[4] = byte(ipUint64 >> 24)
+	b[5] = byte(ipUint64 >> 16)
+	b[6] = byte(ipUint64 >> 8)
+	b[7] = byte(ipUint64)
+	return b
+}
+
+func (enc *IPEncrypter) decodeIPUint64(b []byte) uint64 {
+	return (uint64(b[0]) << 56) | (uint64(b[1]) << 48) | (uint64(b[2]) << 40) | (uint64(b[3]) << 32) | (uint64(b[4]) << 24) | (uint64(b[5]) << 16) | (uint64(b[6]) << 8) | uint64(b[7])
 }
--- encrypt/ip_test.go
+++ encrypt/ip_test.go
@@ -1,84 +1,45 @@
 package encrypt_test
 
 import (
-	"strconv"
 	"testing"
 
 	"github.com/supernarsi/gotool/encrypt"
 )
 
-func TestIPString2Long(t *testing.T) {
-	tests := []struct {
-		in   string
-		want uint
-		err  error
-	}{
-		{"255.255.255.255", 4294967295, nil},
-		{"0.0.0.0", 0, nil},
-		{"0.0.0.0", 0, nil},
-	}
-	for _, tt := range tests {
-		t.Run("test for "+tt.in, func(t *testing.T) {
-			t.Cleanup(func() {})
-			if got, err := encrypt.IPString2Long(tt.in); err != nil {
-				t.Error("got err", err)
-			} else if got != tt.want {
-				t.Errorf("got result %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestLong2IPString(t *testing.T) {
-	type nT struct {
-		in   uint
-		want string
-		err  error
-	}
-	tests := []nT{
-		{100000029, "5.245.225.29", nil},
-		{99999987, "5.245.224.243", nil},
-		{0, "0.0.0.0", nil},
-		{4294967295, "255.255.255.255", nil},
-	}
-	for _, tt := range tests {
-		t.Run("test for "+strconv.Itoa(int(tt.in)), func(t *testing.T) {
-			t.Cleanup(func() {})
-			if got, err := encrypt.Long2IPString(tt.in); err != nil {
-				t.Error("got err", err)
-			} else if got != tt.want {
-				t.Errorf("got result %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestIpEncrypt(t *testing.T) {
 	type nT struct {
 		in   string
 		want string
+		err  error
 	}
 	tests := []nT{
-		{"", ""},
-		{"127.0.0.1", "7f00002b"},
-		{"127.0.0.2", "7f00002c"},
-		{"127.0.0.3", "7f00002d"},
-		{"91.24.21.3", "5b18152d"},
-		{"255.255.255.255", "100000029"},
-		{"255.255.255.254", "100000028"},
-		{"255.255.255.250", "100000024"},
-		{"255.255.255.999", ""},
-		{"101.23.31.999", ""},
-		{"91.202.11.64", "5bca0b6a"},
-		{"2.0.0.0", "200002a"},
-		{"0.0.0.0", "2a"},
-		{"0.0.0.1", "2b"},
-		{"0.0.0.1000", ""},
+		{"none", "", encrypt.ErrIPBeyondIPv4},
+		{"127.0.0.1", "724e6433d351713a", nil},
+		{"127.0.0.2", "724e6433d351713b", nil},
+		{"127.0.0.3", "724e6433d351713c", nil},
+		{"91.24.21.3", "724e6433af69863c", nil},
+		{"255.255.255.255", "724e643454517138", nil},
+		{"255.255.255.254", "724e643454517137", nil},
+		{"255.255.255.250", "724e643454517133", nil},
+		{"255.255.255.999", "", encrypt.ErrIPBeyondIPv4},
+		{"101.23.31.999", "", encrypt.ErrIPBeyondIPv4},
+		{"91.202.11.64", "724e6433b01b7c79", nil},
+		{"2.0.0.0", "724e643356517139", nil},
+		{"0.0.0.0", "724e643354517139", nil},
+		{"0.0.0.1", "724e64335451713a", nil},
+		{"0.0.0.1000", "", encrypt.ErrIPBeyondIPv4},
 	}
 	for _, tt := range tests {
 		t.Run("test for "+tt.in, func(t *testing.T) {
 			t.Cleanup(func() {})
-			if got := encrypt.IpEncrypt(tt.in); got != tt.want {
+			if got, err := encrypt.NewIPEncrypter().Encrypt(tt.in); err != nil {
+				if tt.err == nil || err.Error() != tt.err.Error() {
+					t.Error("got err", err)
+				}
+				if tt.want != "" {
+					t.Error("want should be empty, got", tt.want)
+				}
+			} else if got != tt.want {
 				t.Errorf("got result %v, want %v", got, tt.want)
 			}
 		})
@@ -89,29 +50,36 @@ func TestIpDecrypt2(t *testing.T) {
 	type nT struct {
 		want  string
 		input string
+		err   error
 	}
 	tests := []nT{
-		{"", ""},
-		{"127.0.0.1", "7f00002b"},
-		{"127.0.0.2", "7f00002c"},
-		{"127.0.0.3", "7f00002d"},
-		{"91.24.21.3", "5b18152d"},
-		{"91.202.11.64", "5bca0b6a"},
-		{"2.0.0.0", "200002a"},
-		{"0.0.0.0", "2a"},
-		{"0.0.0.1", "2b"},
-		{"255.255.255.255", "100000029"},
-		{"255.255.255.254", "100000028"},
-		{"255.255.255.250", "100000024"},
-		{"0.0.0.249", "123"},
-		{"0.0.10.146", "abc"},
-		{"", "zzzabc"},
-		{"", "a.c.d"},
+		{"", "", nil},
+		{"127.0.0.1", "724e6433d351713a", nil},
+		{"127.0.0.2", "724e6433d351713b", nil},
+		{"127.0.0.3", "724e6433d351713c", nil},
+		{"91.24.21.3", "724e6433af69863c", nil},
+		{"255.255.255.255", "724e643454517138", nil},
+		{"255.255.255.254", "724e643454517137", nil},
+		{"255.255.255.250", "724e643454517133", nil},
+		{"91.202.11.64", "724e6433b01b7c79", nil},
+		{"2.0.0.0", "724e643356517139", nil},
+		{"0.0.0.0", "724e643354517139", nil},
+		{"0.0.0.1", "724e64335451713a", nil},
+		{"", "123", encrypt.ErrInvalidIPHex},
+		{"", "dafkfo1h23", encrypt.ErrInvalidIPHex},
+		{"", " ", encrypt.ErrInvalidIPHex},
 	}
 	for _, tt := range tests {
 		t.Run("test for "+tt.input, func(t *testing.T) {
 			t.Cleanup(func() {})
-			if got := encrypt.IpDecrypt(tt.input); got != tt.want {
+			if got, err := encrypt.NewIPEncrypter().Decrypt(tt.input); err != nil {
+				if tt.err == nil || err.Error() != tt.err.Error() {
+					t.Error("got err", err)
+				}
+				if tt.want != "" {
+					t.Error("want should be empty, got", tt.want)
+				}
+			} else if got != tt.want || tt.err != nil {
 				t.Errorf("got result %v, want %v", got, tt.want)
 			}
 		})
